8917a8fd66bbf2c4a8491fc33c77d14670a6bd02
b62303411 <samuel.ars@gmail.com>
2025-06-29 09:47:14 -0400

    Create config.php

---
+++ config.php
@@ -0,0 +1,44 @@
+<?php
+$CONFIG = array (
+  'htaccess.RewriteBase' => '/',
+  'memcache.local' => '\\OC\\Memcache\\APCu',
+  'apps_paths' => 
+  array (
+    0 => 
+    array (
+      'path' => '/var/www/html/apps',
+      'url' => '/apps',
+      'writable' => false,
+    ),
+    1 => 
+    array (
+      'path' => '/var/www/html/custom_apps',
+      'url' => '/custom_apps',
+      'writable' => true,
+    ),
+  ),
+  'upgrade.disable-web' => true,
+  'passwordsalt' => 'OZa0Pf563lMVxd8mkzOSyN3ANnfCtJ',
+  'secret' => 'HKvgnRFL2/LGrLwLuqMnnxxcD821qed0U/376fe8GoAvH8Yk',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+    1 => 'b62303411.com',
+    2 => 'nextcloud.b62303411.com',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '31.0.4.1',
+  'overwrite.cli.url' => 'https://nextcloud.b62303411.com',
+  'overwritehost' => 'nextcloud.b62303411.com',
+  'overwriteprotocol' => 'https',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'nextcloud-db',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'n8sBzq43XOsZI0vc1vcNUfB2AFwWYQxV',
+  'installed' => true,
+  'instanceid' => 'ocolyigk1s0p',
+);
